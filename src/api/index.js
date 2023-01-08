542cb47a54562e03fac49e22f105d7b8439650dd
AbdullahLaham <abed26194@gmail.com>
2023-01-08 14:19:01 +0300

    some changes

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import { async } from '@firebase/util';
 import axios from 'axios';
 
-const API = axios.create({baseURL: 'http://localhost:5000/'});
+const API = axios.create({baseURL: 'https://twitter-backend-clone.vercel.app/'});
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('user')) {
