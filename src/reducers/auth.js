98e3a184accd154f5db8b006678444fd1488d70b
AbdullahLaham <abed26194@gmail.com>
2023-01-08 14:23:32 +0300

    some changes

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,10 +1,12 @@
 import { AUTH, LOGOUT, LOGIN, SIGNUP } from "../constants";
-const reducer = (state={authData: null}, action) => {
+const reducer = (state={authData: JSON.parse(localStorage.getItem('user'))}, action) => {
     switch(action.type) {
         case AUTH, SIGNUP, LOGIN: {
+            localStorage.setItem('user', JSON.stringify(action.payload))
             return {...state, authData: action?.payload}
         }
         case LOGOUT: {
+            localStorage.setItem('user', null);
             return {...state, authData: null}
         }
         
